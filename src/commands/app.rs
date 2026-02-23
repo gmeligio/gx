fa9c7c158313faa0faac53f9760593fc7c34bf12
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2026-02-23 09:14:59 +0100

    style: cargo fmt

--- src/commands/app.rs
+++ src/commands/app.rs
@@ -26,7 +26,14 @@ pub fn tidy(repo_root: &Path, manifest_path: &Path, lock_path: &Path) -> Result<
         let lock_store = FileLock::new(lock_path);
         let lock = lock_store.load()?;
         super::tidy::run(
-            repo_root, manifest, manifest_store, lock, lock_store, registry, &scanner, &updater,
+            repo_root,
+            manifest,
+            manifest_store,
+            lock,
+            lock_store,
+            registry,
+            &scanner,
+            &updater,
         )
     } else {
         let action_set = FileWorkflowScanner::new(repo_root).scan_all()?;
@@ -35,7 +42,14 @@ pub fn tidy(repo_root: &Path, manifest_path: &Path, lock_path: &Path) -> Result<
         let lock_store = MemoryLock;
         let lock = Lock::default();
         super::tidy::run(
-            repo_root, manifest, manifest_store, lock, lock_store, registry, &scanner, &updater,
+            repo_root,
+            manifest,
+            manifest_store,
+            lock,
+            lock_store,
+            registry,
+            &scanner,
+            &updater,
         )
     }
 }
@@ -59,7 +73,14 @@ pub fn init(repo_root: &Path, manifest_path: &Path, lock_path: &Path) -> Result<
     let scanner = FileWorkflowScanner::new(repo_root);
     let updater = FileWorkflowUpdater::new(repo_root);
     super::tidy::run(
-        repo_root, manifest, manifest_store, lock, lock_store, registry, &scanner, &updater,
+        repo_root,
+        manifest,
+        manifest_store,
+        lock,
+        lock_store,
+        registry,
+        &scanner,
+        &updater,
     )
 }
 
@@ -85,7 +106,14 @@ pub fn upgrade(
         let lock_store = FileLock::new(lock_path);
         let lock = lock_store.load()?;
         super::upgrade::run(
-            repo_root, manifest, manifest_store, lock, lock_store, registry, &scanner, &updater,
+            repo_root,
+            manifest,
+            manifest_store,
+            lock,
+            lock_store,
+            registry,
+            &scanner,
+            &updater,
             mode,
         )
     } else {
@@ -95,7 +123,14 @@ pub fn upgrade(
         let lock_store = MemoryLock;
         let lock = Lock::default();
         super::upgrade::run(
-            repo_root, manifest, manifest_store, lock, lock_store, registry, &scanner, &updater,
+            repo_root,
+            manifest,
+            manifest_store,
+            lock,
+            lock_store,
+            registry,
+            &scanner,
+            &updater,
             mode,
         )
     }
